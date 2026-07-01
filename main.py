a7873bad349389ecd46e3604a3094e03e0e8495c
Abdulbosid <118301375+AbdulbosidCoder@users.noreply.github.com>
2026-07-01 15:19:58 +0500

    add cors

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # app.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import pandas as pd
 import re
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 pipeline = joblib.load('kategoriya_model_full.pkl')
 
 
